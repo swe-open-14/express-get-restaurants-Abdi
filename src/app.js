d85300c4fd67473395e6a6874afcf413d53f0c04
Abdi Majid <abdi.amajid.dev>
2023-05-10 14:53:08 +0100

    crud functions

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 app.set("json spaces", 2);
+app.use(express.json());
+app.use(express.urlencoded());
 
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants", async (req,res) =>{
@@ -21,6 +23,22 @@ app.get("/restaurants/:id", async (req,res) => {
 res.json(restaurant);
 
 })
+
+app.post("/restaurants/", async (req,res) => {
+    const restaurant = await Restaurant.create(req.body);
+res.json(restaurant);
+})
+
+app.put("/restaurants/:id", async (req,res) => {
+    const UpdatedRestaurant = await Restaurant.update(req.body, {where: {id: req.params.id}});
+res.json(UpdatedRestaurant);
+})
+
+app.delete("/restaurants/:id", async (req,res) => {
+    const deletedRest = await Restaurant.destroy({where: {id: req.params.id}});
+res.json(deletedRest);
+})
+
 
 // app.use()
 
